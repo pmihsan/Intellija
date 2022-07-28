5774bc26a4c8e5cf0f7a9a73b0efc6a99d502577
Mohamed Ihsan <89512452+pmihsan@users.noreply.github.com>
2022-07-28 09:13:30 +0530

    Update Pascal Triangle

--- src/com/ihsan/leetcode/questions/PascalTri.java
+++ src/com/ihsan/leetcode/questions/PascalTri.java
@@ -5,16 +5,20 @@ import java.util.List;
 
 public class PascalTri {
     public static void main(String[] args) {
-//        int rowIndex = 3;
+        int rowIndex = 33;
 //        System.out.println(pascalRow(rowIndex));
         List<List<Integer>> lists = new ArrayList<>();
-        for(int i=0;i<6;i++){
-            lists.add(pascalRow(i));
-//            System.out.println(pascalRow(i));
+        if(rowIndex <= 5) {
+            for (int i = 0; i < rowIndex; i++) {
+                lists.add(pascalRow(i));
+                System.out.println(pascalRow(i));
+            }
         }
-        for(List<Integer> l : lists){
-            System.out.println("Row => " + l);
+        if(rowIndex > 5){
+            correctPascal(lists,rowIndex);
         }
+        printList(lists);
+        System.out.println(lists.get(5));
     }
 
     public static List<Integer> pascalRow(int r){
@@ -33,5 +37,28 @@ public class PascalTri {
             a = a / 10;
         }
         return l;
+    }
+    public static void correctPascal(List<List<Integer>> lists, int r){
+        for (int i = 0; i < r; i++) {
+            lists.add(pascalRow(i));
+        }
+        for(int i=5;i<r;i++){
+            List<Integer> l = new ArrayList<>();
+            l.add(1);
+            for(int j=1;j<i;j++){
+                int a = lists.get(i-1).get(j) + lists.get(i-1).get(j-1);
+                l.add(a);
+//                System.out.print(a+" ");
+            }
+            l.add(1);
+//            System.out.println(l);
+            lists.set(i,l);
+//            System.out.println();
+        }
+    }
+    public static void printList(List<List<Integer>> lists){
+        for(List<Integer> l : lists){
+            System.out.println("Row => " + l);
+        }
     }
 }

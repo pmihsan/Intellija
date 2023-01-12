cee1ef3a2ccce60ed557b366c8db34fb7cc8755c
Mohamed Ihsan <89512452+pmihsan@users.noreply.github.com>
2023-01-12 17:50:48 +0530

    Added File read program

---
+++ src/com/ihsan/practice/ReadFile.java
@@ -0,0 +1,16 @@
+package com.ihsan.practice;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
+public class ReadFile {
+    public static void main(String[] args) throws Exception {
+
+//          WAY 1
+//        FileReader fr = new FileReader("D:\\Pmihsan\\GitHub\\Intellija\\src\\com\\ihsan\\practice\\file.txt");
+//        int i;
+//        while((i=fr.read())!=-1)
+//            System.out.print((char)i);
+//        fr.close();
+    }
+}
